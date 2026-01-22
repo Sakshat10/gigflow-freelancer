204eddce8b568b70d5754d3a97bf87a098e90d11
Sakshat Kumar <sakshat@settlemintindia.com>
2026-01-22 22:22:55 +0530

    Convert all routes and libs to plain JavaScript

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,9 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-var globalForPrisma = global;
-export var prisma = globalForPrisma.prisma ||
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+    globalForPrisma.prisma ||
     new PrismaClient({
         log: ["error"],
     });
+
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
 }
